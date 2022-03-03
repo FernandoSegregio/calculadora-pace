1e829a40ab0bbb3f53fc00e57b0f3c9c7f3d9d78
Fernando Segregio <segregio@gmail.com>
2022-03-02 23:30:30 -0300

    feat + style: cria app com com um forms com 3 campos inputs e um botao, cria funcao para pegar o valor digitado em cada campo. Cria Styled component para o app.

--- src/components/CalcPaceTemp.jsx
+++ src/components/CalcPaceTemp.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import CountainerStyle from './style';
 
 export default function CalcPaceTemp() {
+  const [distance, setDistance] = useState('');
+  const [min, setMin] = useState('');
+  const [seg, setSeg] = useState('');
+
+  function handleIputDist(target) {
+    setDistance(target.value);
+  }
+
+  function handleIputMin(target) {
+    setMin(target.value);
+  }
+
+  function handleIputSeg(target) {
+    setSeg(target.value);
+  }
+
   return (
-    <div>CalcPaceTemp</div>
-  )
+    <CountainerStyle>
+      <h3>CALCULA TEMPO TOTAL DE ACORDO COM O PACE</h3>
+      <form action="">
+        <label htmlFor="km">
+          Distância (em Km)
+          <input type="text" value={distance} onChange={({ target }) => handleIputDist(target)} name="distance" id="km" placeholder="Km" />
+        </label>
+        <div className="min-seg">
+          Pace por Km
+          <label htmlFor="for-km" className="for-km">
+            <input type="number" value={min} onChange={({ target }) => handleIputMin(target)} id="for-km" placeholder="Minutos" />
+            <input type="number" value={seg} onChange={({ target }) => handleIputSeg(target)} placeholder="Segundos" />
+          </label>
+        </div>
+        <button type="button" onClick={}>Calcular</button>
+      </form>
+      <h3>
+        Tempo total:
+        {' '}
+        {calcTempHour}
+        h
+        {' '}
+        {calcTempMin}
+        min
+        {' '}
+      </h3>
+    </CountainerStyle>
+  );
 }
---
+++ src/components/style.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+const CountainerStyle = styled.div`
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    height: 330px;
+    background: var(--gray);
+    border-radius: 15px;  
+    margin-left: auto;
+    margin-right: auto;
+    color: var(--yellow);
+
+    H3 {
+      text-align: center;
+      padding: 23px;
+      color: var(--yellow);
+      font-size: 1.3rem;
+    }
+
+    label {
+      display: flex;
+      flex-direction: column;
+      column-gap: 4px;
+    }
+
+    .min-seg {
+      align-items: flex-start;
+    }
+
+    .for-km {
+      flex-direction: row;
+    }
+
+    input {
+      height: 40px;
+      background-color: var(--grayLight);
+      border: none;
+      border-radius: 6px;
+    }
+
+    input::placeholder {
+      color: var(--gray);
+      text-align: center;
+      font-size: 16px;
+    }
+
+    button {
+      margin-top: 10px;
+      height: 40px;
+      background-color: var(--black);
+      border: none;
+      border-radius: 6px;
+      width: 100%;
+      color: var(--yellow);
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export default CountainerStyle;
